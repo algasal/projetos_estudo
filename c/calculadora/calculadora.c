1e6621ce4511a77162ad76e3e426ac08ae566e3f
algasal <albuquerque.g.aldo@gmail.com>
2023-07-28 22:39:12 -0300

    Calculadora v2 - agora respeitando prioridade de operações

--- c/calculadora/calculadora.c
+++ c/calculadora/calculadora.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define MAXIMO 100
 
-/* Calculadora simples, que executa uma ou mais equações sem respeitar prioridade de operações */
+/* Calculadora simples, que executa uma ou mais equação, respeitando a prioridade das operações, mas sem respeitar parênteses */
 
 float calculadora(float x, float y, char sinal);
 float resolver_equacao(char equacao[]);
+int precedencia_sinais(char sinal);
 float soma(float x, float y) { return x + y; }
 float subt(float x, float y) { return x - y; }
 float mult(float x, float y) { return x * y; }
@@ -24,13 +26,11 @@ main(int argc, char *argv[])
 {
     char equacao[MAXIMO];
 
-    printf("Bem vindo à calculadora v1!\n");
+    printf("Bem vindo à calculadora v2!\n");
     printf("Digite a sua equacao: ");
     fgets(equacao, MAXIMO, stdin);
     
-    float resultado = resolver_equacao(equacao);
-
-    printf("resultado: %.1f\n", resultado);
+    printf("resultado: %.1f\n", resolver_equacao(equacao));
 
     return 0;
 }
@@ -39,28 +39,60 @@ main(int argc, char *argv[])
 float
 resolver_equacao(char equacao[]) 
 {
-    float x, y, resultado;
-    char *sinal;
+    float x, y, resultado, stack[100];
+    char sinal, stack_sinais[100];
+    int i, stack_top = -1, sinais_top = -1;
 
-    /* token usado pra quebrar a equação em pedaços menores de 'num', 'sinal', 'num' */
-    char *token = strtok(equacao, " \n");
-    x = atoi(token);
-
-    while (token != NULL) {
-        sinal = strtok(NULL, " \n");
-        token = strtok(NULL, " \n");
-        
-        if (token != NULL) {
-            y = atoi(token);
-
-            resultado = calculadora(x, y, sinal[0]);
-            printf("x: %.1f, y: %.1f, op: %c\n", x, y, sinal[0]);
-
-            x = resultado;
+    for (i = 0; equacao[i]; i++) {
+        /* se o caracter for um espaço em branco, pula */
+        if (equacao[i] == ' ') {
+            continue;
+        } else if (isdigit(equacao[i])) {
+            float num = 0;
+            /* while necessário p caso o número tenha + de 1 dígito */           
+            while (isdigit(equacao[i])) {
+                /* transforma o caracter equacao[i] em um número */
+                num = num * 10 + (equacao[i] - '0');
+                i++;
+            }
+            i--;
+            
+            /* coloca o número obtido no topo do stack */
+            stack[++stack_top] = num;
+        } else if (equacao[i] == '+' || equacao[i] == '-' || equacao[i] == '*' || equacao[i] == '/') {
+            /* se a precedencia do sinal no topo for maior que a do sinal lido, 
+             * realiza a operação no topo do stack com os dois números do topo
+             * depois coloca o resultado da operação no topo do stack, e o sinal lido no topo do stack de sinais */
+            while(sinais_top >= 0 && precedencia_sinais(stack_sinais[sinais_top]) >= precedencia_sinais(equacao[i])) {
+                 x = stack[stack_top--];
+                 y = stack[stack_top--];
+                 sinal = stack_sinais[sinais_top--];
+                 stack[++stack_top] = calculadora(x, y, sinal);
+            }
+            stack_sinais[++sinais_top] = equacao[i];
         }
     }
 
+    /* faz os cálculos das equações que sobrarem */
+    while (sinais_top >= 0) {
+        y = stack[stack_top--];
+        x = stack[stack_top--];
+        sinal = stack_sinais[sinais_top--];
+        stack[++stack_top] = calculadora(x, y, sinal);
+    }
+
+    resultado = stack[0];
     return resultado;
+}
+
+/* checa a precedência das equações */
+int
+precedencia_sinais(char sinal)
+{
+    if (sinal == '+' || sinal == '-') return 1;
+    else if (sinal == '*' || sinal == '/') return 2;
+    
+    return 0;
 }
 
 /* realiza o cálculo em si */
@@ -74,17 +106,10 @@ calculadora(float x, float y, char sinal)
         {'/', divs},
     };
 
-    float resultado;
-
     /* loopa pela array operacoes[] pra encontrar a operação correspondente ao sinal */
     for (int i = 0; i < sizeof(operacoes) / sizeof(operacoes[0]); i++) {
         if (operacoes[i].Key == sinal) {
-            resultado = operacoes[i].Func(x, y);
-            break;
+            return operacoes[i].Func(x, y);
         }
     }
-
-    return resultado;
 }
-
- 
